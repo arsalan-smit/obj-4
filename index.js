842495937be9d89d4116b17c0b608a047f04b63d
arsalan-smit <arsalangg05@gmail.com>
2024-10-21 18:19:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var firstVar, secondVar, thirdVar;
+//legal
+var $myVar, _variable, firstName, name1, myVariable;
+//illegal
+// var 1stVar, @name, first-name, var, true;
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("A heading stating <q>Rules for naming JS variables</q>");
+document.write("<p>Variable names can only contain <u>letters</u>, <u>numbers</u>, <u>$</u>, and <u>_.</u> For example: <code>$my_1stVariable</code>.</p>");
+document.write("<p>Variable names are case sensitive.</p>");
+document.write("<p>Variable names should not be JavaScript reserved words.</p>");
+
+
